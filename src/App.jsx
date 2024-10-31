3125123205ff0d1cb8255a9b5fad615048acfd4a
Jessada Khaoseengam <tongetoon@gmail.com>
2024-10-31 19:15:33 +0000

    adding m2 model via spline

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Spline from '@splinetool/react-spline';
+
+function App() {
+
+  return (
+    <>
+      <div className='content'>
+        <h1>Sheer Driving Pleasure</h1>
+        <Spline scene="https://prod.spline.design/Sn-5lUE8m9FYo0ve/scene.splinecode" />
+      </div>
+    </>
+  )
+}
+
+export default App
