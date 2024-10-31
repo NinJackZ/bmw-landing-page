973ad795482308640ab7a0195cc3e1c53e892c98
Jessada Khaoseengam <tongetoon@gmail.com>
2024-10-31 18:35:13 +0000

    foundations

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='content'>
+        <h1>Sheer Driving Pleasure</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
